79197f83fafe495f5aef791b014dabff138e51d6
zone1240 <z_zone1240@163.com>
2020-03-20 23:03:19 +0800

    <update>1160.拼写单词

--- 1160_find-words-that-can-be-formed-by-characters.js
+++ 1160_find-words-that-can-be-formed-by-characters.js
@@ -1,4 +1,4 @@
-// 1160.拼写单词
+// 1160.拼写单词（简单）
 /*
 给你一份『词汇表』（字符串数组） words 和一张『字母表』（字符串） chars。
 假如你可以用 chars 中的『字母』（字符）拼写出 words 中的某个『单词』（字符串），那么我们就认为你掌握了这个单词。
@@ -21,11 +21,29 @@
 1 <= words.length <= 1000
 1 <= words[i].length, chars.length <= 100
 所有字符串中都仅包含小写英文字母
-
-来源：力扣（LeetCode）
-链接：https://leetcode-cn.com/problems/find-words-that-can-be-formed-by-characters
-著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
 */
+
+// 优秀源码
+// 方法一：哈希表记数（Python）
+// 思路和算法：
+// 显然，对于一个单词 word，只要其中的每个字母的数量都不大于 chars 中对应的字母的数量，那么就可以用 chars 中的字母拼写出 word。所以我们只需要用一个哈希表存储 chars 中每个字母的数量，再用一个哈希表存储 word 中每个字母的数量，最后将这两个哈希表的键值对逐一进行比较即可。
+
+// class Solution:
+//     def countCharacters(self, words: List[str], chars: str) -> int:
+//         chars_cnt = collections.Counter(chars)
+//         ans = 0
+//         for word in words:
+//             word_cnt = collections.Counter(word)
+//             for c in word_cnt:
+//                 if chars_cnt[c] < word_cnt[c]:
+//                     break
+//             else:
+//                 ans += len(word)
+//         return ans
+
+// 复杂度分析
+// 时间复杂度：O(n)O(n)，其中 nn 为所有字符串的长度和。我们需要遍历每个字符串，包括 chars 以及数组 words 中的每个单词。
+// 空间复杂度：O(S)O(S)，其中 SS 为字符集大小，在本题中 SS 的值为 2626（所有字符串仅包含小写字母）。程序运行过程中，最多同时存在两个哈希表，使用的空间均不超过字符集大小 SS，因此空间复杂度为 O(S)O(S)。
 
 /**
  * @param {string[]} words
