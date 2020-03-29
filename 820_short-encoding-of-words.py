a201d2699a57ca0cb9cb7e924294c69495f9701a
zone1240 <z_zone1240@163.com>
2020-03-29 22:45:18 +0800

    <update>820. 单词的压缩编码

--- 820_short-encoding-of-words.py
+++ 820_short-encoding-of-words.py
@@ -25,15 +25,14 @@
 预备知识——字典树
 
 方法一：存储后缀
-思路
+思路：
 如果单词 X 是 Y 的后缀，那么单词 X 就不需要考虑了，因为编码 Y 的时候就同时将 X 编码了。例如，如果 words 中同时有 "me" 和 "time"，我们就可以在不改变答案的情况下不考虑 "me"。
 如果单词 Y 不在任何别的单词 X 的后缀中出现，那么 Y 一定是编码字符串的一部分。
 因此，目标就是保留所有不是其他单词后缀的单词，最后的结果就是这些单词长度加一的总和，因为每个单词编码后后面还需要跟一个 # 符号。
 
-算法
+算法：
 由数据范围可知一个单词最多含有 7 个后缀，所以我们可以枚举单词所有的后缀。对于每个后缀，如果其存在 words 列表中，我们就将其从列表中删除。为了高效删除，我们将 words 用哈希集合（HashSet）来存储。
 
-'''python
 class Solution:
     def minimumLengthEncoding(self, words: List[str]) -> int:
         good = set(words)
@@ -42,16 +41,10 @@ class Solution:
                 good.discard(word[k:])
 
         return sum(len(word) + 1 for word in good)
-'''
 
-复杂度分析
+复杂度分析：
 
-时间复杂度：O(\sum w_i^2)O(∑w 
-i
-2
-​	
- )，其中 w_iw i 是 words[i] 的长度。每个单词有 w_iw 
-i个后缀，对于每个后缀，查询其是否在集合中时需要进行 O(w_i)O(w i) 的哈希值计算。
+时间复杂度：O(\sum w_i^2)，其中 w_i 是 words[i] 的长度。每个单词有 w_i 个后缀，对于每个后缀，查询其是否在集合中时需要进行 O(w_i)O(w i) 的哈希值计算。
 空间复杂度：O(\sum w_i)O(∑w i)，存储单词的空间开销。
 
 方法二：字典树
@@ -61,7 +54,7 @@ i个后缀，对于每个后缀，查询其是否在集合中时需要进行 O(w
 算法
 去找到是否不同的单词具有相同的后缀，我们可以将其反序之后插入字典树中。例如，我们有 "time" 和 "me"，可以将 "emit" 和 "em" 插入字典树中。
 然后，字典树的叶子节点（没有孩子的节点）就代表没有后缀的单词，统计叶子节点代表的单词长度加一的和即为我们要的答案。
-'''python
+
 class Solution:
     def minimumLengthEncoding(self, words: List[str]) -> int:
         words = list(set(words)) #remove duplicates
@@ -78,10 +71,9 @@ class Solution:
         return sum(len(word) + 1
                    for i, word in enumerate(words)
                    if len(nodes[i]) == 0)
-'''
+
 复杂度分析
 时间复杂度：O(∑wi)，其中 wi 是 words[i] 的长度。对于每个单词中的每个字母，只需要进行常数次操作。
-
 空间复杂度：O(S∗∑w i)，字典树的空间开销，其中 S 为字符集大小。
 
 作者：LeetCode-Solution
@@ -89,3 +81,19 @@ class Solution:
 来源：力扣（LeetCode）
 著作权归作者所有。商业转载请联系作者获得授权，非商业转载请注明出处。
 '''
+
+# 解题思路
+# 1、discard函数会删去set中的元素，如果不存在，不会报错
+# 2、只要确定set中没有重复单词即可
+
+# Python3 代码
+class Solution:
+    def minimumLengthEncoding(self, words: List[str]) -> int:
+        word=set(words)
+        sum=0
+        for i in words:
+            for j in range(1, len(i)):
+                word.discard(i[j:])
+        for k in word:
+            sum+=len(k)+1
+        return sum
